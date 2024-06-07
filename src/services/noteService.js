75dd234cf60158d56609771eb21a3cbb7e29e7f2
jabss <jabrijuhinin93@gmail.com>
2024-06-08 00:51:44 +0800

    change baseUrl (now dev mode not working)

--- src/services/noteService.js
+++ src/services/noteService.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 // const baseUrl = 'https://localhost:3001/api/notes';
-const baseUrl = 'http://localhost:3001/api/notes'
+const baseUrl = '/api/notes'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
