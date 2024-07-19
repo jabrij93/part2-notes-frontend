941ad3d186150591f58d5642810ccdde7d23b068
jabss <jabrijuhinin93@gmail.com>
2024-07-19 23:46:56 +0800

    logged user's login info in browser's local storage

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,15 @@ const App = () => {
       })
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      noteService.setToken(user.token)
+    }
+  }, [])
+
   const addNote = (event) => {
     event.preventDefault()
 
@@ -50,6 +59,9 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
+      window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      ) 
       noteService.setToken(user.token)
       setUser(user)
       setUsername('')
