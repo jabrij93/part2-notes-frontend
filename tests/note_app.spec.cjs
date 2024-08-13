93dfddc42cf4d772ea8314cd0db69f3ba8e9b477
jabss <jabrijuhinin93@gmail.com>
2024-08-14 03:54:12 +0800

    update test, test now working

--- tests/note_app.spec.cjs
+++ tests/note_app.spec.cjs
@@ -4,6 +4,6 @@ describe('Note app', () => {
     test('front page can be opened', async ({ page }) => {
         await page.goto('http://localhost:5173')
 
-        await page.getByRole('button', { name: 'log in' }).click()
+        await page.getByRole('button', { name: 'login' }).click()
     })
-  })``
+  })
