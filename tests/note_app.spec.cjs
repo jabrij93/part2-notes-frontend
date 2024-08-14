724dcbfb00890ef017701ad558cf6030e1ecd174
jabss <jabrijuhinin93@gmail.com>
2024-08-15 01:45:56 +0800

    test add note while login working but if same test is run twice test failed

--- tests/note_app.spec.cjs
+++ tests/note_app.spec.cjs
@@ -1,6 +1,10 @@
-const { test, describe, expect } = require('@playwright/test')
+const { test, describe, expect, beforeEach } = require('@playwright/test')
 
 describe('Note app', () => {
+    beforeEach(async ({ page }) => {
+        await page.goto('http://localhost:5173')
+    })
+
     test('front page can be opened', async ({ page }) => {
         await page.goto('http://localhost:5173')
     
@@ -9,9 +13,7 @@ describe('Note app', () => {
         await expect(page.getByText('Note app, Department of Computer Science, University of Helsinki 2024')).toBeVisible()
     })
 
-    test('login form can be opened', async ({ page }) => {
-        await page.goto('http://localhost:5173')
-    
+    test('user can log in', async ({ page }) => {
         await page.getByRole('button', { name: 'login' }).click()
         await page.getByTestId('username').fill('mluukkai')
         await page.getByTestId('password').fill('salainen')
@@ -19,4 +21,20 @@ describe('Note app', () => {
   
         await expect(page.getByText('mluukkai logged in')).toBeVisible()
     })
+
+    describe('when logged in', () => {
+        beforeEach(async ({ page }) => {
+          await page.getByRole('button', { name: 'login' }).click()
+          await page.getByTestId('username').fill('mluukkai')
+          await page.getByTestId('password').fill('salainen')
+          await page.getByRole('button', { name: 'login' }).click()
+        })
+    
+        test('a new note can be created', async ({ page }) => {
+          await page.getByRole('button', { name: 'add note' }).click()
+          await page.getByRole('textbox').fill('a note created by playwright3')
+          await page.getByRole('button', { name: 'save' }).click()
+          await expect(page.getByText('a note created by playwright3')).toBeVisible()
+        })
+      }) 
 })
