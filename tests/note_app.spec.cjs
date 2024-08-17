cba69440a413d607231828ebd08cb3b0b87f6f8e
jabss <jabrijuhinin93@gmail.com>
2024-08-17 21:36:05 +0800

    configured baseURL to 'localhost:5173'

--- tests/note_app.spec.cjs
+++ tests/note_app.spec.cjs
@@ -3,8 +3,8 @@ const { loginWith, createNote } = require('./helper.cjs')
 
 describe('Note app', () => {
     beforeEach(async ({ page, request }) => {
-      await request.post('http://localhost:3001/api/testing/reset')
-      await request.post('http://localhost:3001/api/users', {
+      await request.post('/api/testing/reset')
+      await request.post('/api/users', {
         data: {
           name: 'Matti Luukkainen',
           username: 'mluukkai',
@@ -12,11 +12,11 @@ describe('Note app', () => {
         }
       })
 
-      await page.goto('http://localhost:5173')
+      await page.goto('/')
     })
 
     test('front page can be opened', async ({ page }) => {
-        await page.goto('http://localhost:5173')
+        await page.goto('/')
     
         const locator = await page.getByText('Notes')
         await expect(locator).toBeVisible()
@@ -29,27 +29,42 @@ describe('Note app', () => {
         await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
     })
 
-    describe.only('when logged in', () => {
+    describe('when logged in', () => {
         beforeEach(async ({ page }) => {
           await loginWith(page, 'mluukkai', 'salainen')
         })
     
         test('a new note can be created', async ({ page }) => {
-            await createNote(page, 'a note created by playwright')
-            await expect(page.getByText('a note created by playwright')).toBeVisible()
-          })
+            await createNote(page, 'a note created by playwright8')
+            await expect(page.getByText('a note created by playwright8')).toBeVisible()
         })
 
         describe('and a note exists', () => {
           beforeEach(async ({ page }) => {
-            await createNote(page, 'another note by playwright')
+              await createNote(page, 'another note by playwright8')
           })
-
+    
           test('it can be made important', async ({ page }) => {
-            await page.getByRole('button', { name: 'make not important' }).click()
-            await expect(page.getByText('make important')).toBeVisible()
+              await page.getByRole('button', { name: 'make not important' }).click()
+              await expect(page.getByText('make important')).toBeVisible()
           })
         })
+
+        describe('and several notes exists', () => {
+          beforeEach(async ({ page }) => {
+            await createNote(page, 'first note', true)
+            await createNote(page, 'second note', true)
+          })
+      
+          test('one of those can be made non important', async ({ page }) => {
+            const otherNoteElement = await page.getByText('first note')
+      
+            await otherNoteElement
+              .getByRole('button', { name: 'make not important' }).click()
+            await expect(otherNoteElement.getByText('make important')).toBeVisible()
+          })
+        })
+    })
       
     test('login fails with wrong password', async ({ page }) => { 
         await page.getByRole('button', { name: 'login' }).click()
@@ -67,5 +82,5 @@ describe('Note app', () => {
         await expect(errorMessage).toHaveCSS('color', 'rgb(255, 0, 0)'); // 'red' in RGB format
 
         await expect(page.getByText('Matti Luukkainen logged in')).not.toBeVisible()
-      })  
+    })
 })
